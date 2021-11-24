66ffa7d14a10b91ba23391874ba862eca866e135
Shawn Hou <shawn_hou@163.com>
2021-11-24 20:37:57 +0800

    'springboot初始化'

---
+++ src/main/java/com/dclingcloud/kubetopo/KubeTopoApplication.java
@@ -0,0 +1,13 @@
+package com.dclingcloud.kubetopo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeTopoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubeTopoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dclingcloud/kubetopo/KubeTopoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dclingcloud.kubetopo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeTopoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
