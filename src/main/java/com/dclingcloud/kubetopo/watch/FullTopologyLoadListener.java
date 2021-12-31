e20769c860b6eb3f985adda8d692b291b9e01d5a
dokeyhou <dokeyhou@tencent.com>
2021-12-31 15:22:03 +0800

    日志记录启动事件

--- src/main/java/com/dclingcloud/kubetopo/watch/FullTopologyLoadListener.java
+++ src/main/java/com/dclingcloud/kubetopo/watch/FullTopologyLoadListener.java
@@ -3,6 +3,7 @@ package com.dclingcloud.kubetopo.watch;
 import com.dclingcloud.kubetopo.service.TopologyService;
 import com.dclingcloud.kubetopo.util.K8sServiceException;
 import io.kubernetes.client.openapi.ApiException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ import javax.annotation.Resource;
 import java.util.concurrent.locks.ReentrantLock;
 
 @Component
+@Slf4j
 public class FullTopologyLoadListener implements InitializingBean {
     @Resource
     private TopologyService topologyService;
@@ -18,7 +20,9 @@ public class FullTopologyLoadListener implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        log.info("Loading full topology of K8s resources");
         reload();
+        log.info("Loaded full topology of K8s resources, start watching events");
     }
 
     public boolean reload() throws ApiException, K8sServiceException {
